b7d1c7dab0c55cd09d2499ac3b861efb28a0c3fc
Miklos Juhasz <juhasz.miklos.1@gmail.com>
2013-12-28 13:32:27 +0100

    Update year in copyright headers.

--- src/main/java/bdsup2sub/gui/support/ActionMenu.java
+++ src/main/java/bdsup2sub/gui/support/ActionMenu.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Miklos Juhasz (mjuhasz), JetBrains s.r.o.
+ * Copyright 2014 Miklos Juhasz (mjuhasz), JetBrains s.r.o.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
